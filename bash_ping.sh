a2c9b9adaa7507771cc111286109f9d08e62c155
Regi0 <dhondt.reginald@hotmail.com>
2024-11-27 18:17:08 +0100

    done basic objective

---
+++ bash_ping.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ping -DOUvT tsprespec -c4 192.168.1.63 >> /home/regi/Bash_ping/Testresults/Result1.txt &
+ping -DOUvT tsprespec -c4 127.0.0.1 >> /home/regi/Bash_ping/Testresults/Result2.txt &
+
+cat /home/regi/Bash_ping/Testresults/Result1.txt >> /home/regi/Bash_ping/Testresults/Endresult.txt
+cat /home/regi/Bash_ping/Testresults/Result2.txt >> /home/regi/Bash_ping/Testresults/Endresult.txt
+
+> /home/regi/Bash_ping/Testresults/Result1.txt
+> /home/regi/Bash_ping/Testresults/Result2.txt
