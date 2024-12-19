bf29cea8fb7a7e3f94568b02ab9e48f408e23e08
Regi0 <dhondt.reginald@hotmail.com>
2024-12-19 20:40:32 +0100

    Features-done

--- bash_ping.sh
+++ bash_ping.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Get the current timestamp for the output file
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+OUTPUT_FILE="/home/regi/Bash_ping/Testresults/$TIMESTAMP.txt"
+
 ping -DOUvT tsprespec -c4 192.168.1.63 >> /home/regi/Bash_ping/Testresults/Result1.txt &
 ping -DOUvT tsprespec -c4 127.0.0.1 >> /home/regi/Bash_ping/Testresults/Result2.txt &
 
-cat /home/regi/Bash_ping/Testresults/Result1.txt >> /home/regi/Bash_ping/Testresults/Endresult.txt
-echo -e "\n----- Separator -----\n" >> /home/regi/Bash_ping/Testresults/Endresult.txt
-cat /home/regi/Bash_ping/Testresults/Result2.txt >> /home/regi/Bash_ping/Testresults/Endresult.txt
+cat /home/regi/Bash_ping/Testresults/Result1.txt >> "/home/regi/Bash_ping/Testresults/$TIMESTAMP.txt"
+echo -e "\n----- Separator -----\n" >> "/home/regi/Bash_ping/Testresults/$TIMESTAMP.txt"
+cat /home/regi/Bash_ping/Testresults/Result2.txt >> "/home/regi/Bash_ping/Testresults/$TIMESTAMP.txt"
 
 > /home/regi/Bash_ping/Testresults/Result1.txt
 > /home/regi/Bash_ping/Testresults/Result2.txt
--- readme.md
+++ readme.md
@@ -6,3 +6,5 @@ Note down the answer in a file
 Features:
 Name the file with a timestamp of when you did the test
 Let it run parallel, to make it faster
+-> ok
+
